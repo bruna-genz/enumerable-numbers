adc3da6ff704f4b8b9465f90be150f0fb19dc837
Bruna <brunagenz92@gmail.com>
2019-09-12 18:27:30 -0300

    created #my_each_with_index and #my_select methods

--- enumerable-number.rb
+++ enumerable-number.rb
@@ -1,11 +1,28 @@
 module Enumerable
-    def my_each
-      for i in 0...self.length
-        yield(self[i])
-      end
+  def my_each
+    i = 0
+    while i < self.length
+      yield(self[i])
+      i += 1
     end
+  end
 
-    
+  def my_each_with_index
+    i = 0
+    while i < self.length
+      yield(self[i], i)
+      i += 1
+    end
+  end
+
+  def my_select
+    result = []
+    self.my_each do |element|
+      result << element if yield(element) 
+    end
+    result
+  end
+        
 
 end
 
