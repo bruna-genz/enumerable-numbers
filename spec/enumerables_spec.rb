53597de6fc6124a7419910ba722b170828ba845e
Bruna <brunagenz92@gmail.com>
2019-10-03 19:05:35 -0300

    created test cases for #my_select method

--- spec/enumerables_spec.rb
+++ spec/enumerables_spec.rb
@@ -20,4 +20,20 @@ RSpec.describe Enumerable do
       expect(output).to eql([1, 4, 9, 16])
     end
   end
+
+  describe "my_select" do
+    let(:array) { [1, 2, 3, 4, 5, 6] }
+
+    it "returns all items which the given block returns a true value" do
+      output = []
+      array.my_select { |element| output << element if element.even? }
+      expect(output).to eql([2, 4, 6])
+    end
+
+    it "returns an enumerable if no code block is passed" do
+      expect(array.my_select).to be_a(Enumerable)
+    end
+  end
+
+  
 end
